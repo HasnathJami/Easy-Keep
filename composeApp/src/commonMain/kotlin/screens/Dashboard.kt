7f3e8971ac24461faccf5adead9294f661825855
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-05-09 01:57:18 +0600

    initial commit

---
+++ composeApp/src/commonMain/kotlin/screens/Dashboard.kt
@@ -0,0 +1,27 @@
+package screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun DashboardScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(
+            text = "Dashboard",
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+
+}
